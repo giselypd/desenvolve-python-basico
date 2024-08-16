c56c78756528432b9da6e13f7824b2e01b20a47f
Gisely Thawane <gisely.ferreira@pdbomdespacho.com.br>
2024-08-16 17:44:19 -0300

    Add files via upload

---
+++ modulo2/2.2 - Tipos de Dados/aula2_questao2.py
@@ -0,0 +1,17 @@
+#Armazene o texto "o resultado é"  em uma variável, o valor 10 em outra variável, e o valor 3.5 numa terceira variável.
+#Some os valores da segunda e terceira variável e armazene em outra variável.
+#Imprima todas as variáveis na ordem de criação e imprima também seus tipos.
+
+resultado = "o resultado é "
+v1 = 10
+v2 = 3.5
+
+soma= v1 + v2
+print(resultado)
+print(v1)
+print(v2)
+print(soma)
+print(type(resultado))
+print(type(v1))
+print(type(v2))
+print(type(soma))
