df434628d5b6881edc7ec55883773c2e1d46655f
Gisely Thawane <gisely.ferreira@pdbomdespacho.com.br>
2024-10-18 15:36:53 -0300

    Delete modulo6/6.2 - Operações e Funções/6.3 - Fatiamento directory

--- modulo6/6.2 - Operações e Funções/6.3 - Fatiamento/aula3_questao2.py
+++
@@ -1,6 +0,0 @@
-urls = ["www.google.com", "www.gmail.com", "www.github.com", "www.reddit.com", "www.yahoo.com"]
-dominios = []
-for url in urls:
-    dominios.append(url[4:-4])
-    
-print(dominios)
